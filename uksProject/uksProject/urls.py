44cb74c9b33f4b30d5fb6ec055d81f0b808b839d
Nikola Milekic <milekic.nikola@gmail.com>
2018-04-21 05:04:02 +0200

    Ukljuceni registration i bootstrap3 moduli, napravljena pocetna stranica, registracija, log-in i log-out

--- uksProject/uksProject/urls.py
+++ uksProject/uksProject/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'uksProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('uksApp.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+)
